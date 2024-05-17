4d91550ef2817f2243130e1fd1469297be7e0d94
Stckrz <rin449@gmail.com>
2024-05-17 17:15:31 -0500

    butts

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 
 	return (
 		<>
-			<Router>
+			<Router basename={"/calleditprod/"}>
 				<Routes>
 					{/* <Route path="/" element={<Home />}> */}
 						<Route index element={<Home />} />
