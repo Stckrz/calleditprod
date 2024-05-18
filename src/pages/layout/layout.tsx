5c387e5224d2fce6febc14b49c8de5368c34b843
Stckrz <rin449@gmail.com>
2024-05-17 21:06:09 -0500

    aaah

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -6,9 +6,7 @@ interface LayoutProps {
 }
 const Layout: React.FC<LayoutProps> = ({ children }) => {
 	return (
-		// <body className={"flex flex-col"}>
-		// <div className={"flex flex-col h-screen w-screen"}> 
-		<div className={"flex flex-col"}>
+		<div className={"flex flex-col h-full"}>
 			<div>
 				<Navbar />
 			</div>
